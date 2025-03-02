3938d075d298c834cf60a6f368081bc0eea8fd76
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-03-02 03:45:28 +0300

    my saved books page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import LoginPage from "./app/pages/LoginPage.tsx"
 import Register from "./app/pages/Register.tsx"
 import HomePage from "./app/pages/HomePage.tsx"
 import SearchPage from "./app/pages/SearchPage.tsx";
+import MyBooks from "./app/pages/MyBooks.tsx"
 function App() {
   return (
       <>
@@ -14,6 +15,7 @@ function App() {
               <Route path={"/"} element={<GetStarted/>}/>
               <Route path={"/home"} element={<HomePage/>}/>
               <Route path={"/search"} element={<SearchPage/>}/>
+              <Route path={"/saved-books"} element={<MyBooks/>}/>
           </Routes>
       </>
 
--- src/app/pages/MyBooks.tsx
+++ src/app/pages/MyBooks.tsx
@@ -1,7 +1,56 @@
+import {useEffect, useState} from "react";
+import BookCard from "../components/BookCard.tsx";
+import Modal from "../components/Modal.tsx";
+
+type Book ={
+    id:number |string,
+    title:string,
+    imageCover:string,
+    author:string,
+    genre:string
+}
 const MyBooks =()=>{
+    const userId :number=1
+    const[message,setMessage] =useState<string>("")
+    const[modalColor,setModalColor] =useState<string>("green")
+    const[books,setAllBooks] =useState<Book[]>([])
+    useEffect(() => {
+        fetchAllSavedBooks()
+    }, []);
+    const fetchAllSavedBooks =(async ()=>{
+        try{
+            const response=await fetch(import.meta.env.VITE_STORYBOOK_BASE_URL+`/api/v1/users/${userId}/saved-books`,{
+                method:"GET"
+            })
+            if(response.ok){
+                const data =await response.json()
+                if(data?.statusCode !=200){
+                    setMessage(data?.message)
+                }else{
+                    setAllBooks(data?.bookList)
+                }
+            }
+        }catch (e) {
+            setModalColor("red")
+            setMessage(" Error: "+e)
+        }
+
+    })
     return(
         <div className={"my__books"}>
-
+            <h1>My Saved books</h1>
+            <div>
+                {books?.length>0?(
+                    books?.map(({id,title,imageCover,author,genre}:Book,index:number)=>(
+                        <div key={index} id={id.toString()}>
+                            <BookCard category={genre} image={imageCover} title={title} author={author} />
+                        </div>
+                    ))
+                ):(
+                    <div>Book list is empty</div>
+                )}
+            </div>
+            {message&&<Modal message={message} color={modalColor} />}
         </div>
     )
 }
